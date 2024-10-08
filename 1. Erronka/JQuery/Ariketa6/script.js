515499056a5bbb413702f236b128bfebdf28e229
asierferrero <asierferrero09@gmail.com>
2024-10-08 10:58:06 +0200

    ariketa6 hobetua

--- 1. Erronka/JQuery/Ariketa6/script.js
+++ 1. Erronka/JQuery/Ariketa6/script.js
@@ -2,17 +2,54 @@ $(document).ready(function () {
     $(document).keydown(function (e) {
         switch (e.key) {
             case "ArrowLeft":
-                $('#rocket').animate({left:"-=100px"}, 500);
+                $('#rocket').css({ 'transform': 'rotate(0deg)' })
+                if (e.ctrlKey) {
+                    $('#rocket')
+                        .css({ 'transform': 'rotate(-45deg)' })
+                        .animate({ top: "-=100px", left: "-=100px" }, 500);
+                } else if (e.shiftKey) {
+                    $('#rocket')
+                        .css({ 'transform': 'rotate(-135deg)' })
+                        .animate({ top: "+=100px", left: "-=100px" }, 500);
+                } else {
+                    $('#rocket')
+                        .css({ 'transform': 'rotate(-90deg)' })
+                        .animate({ left: "-=100px" }, 500);
+                }
                 break;
             case "ArrowRight":
-                $('#rocket').animate({left:"+=100px"}, 500);
+                $('#rocket').css({ 'transform': 'rotate(0deg)' })
+                if (e.ctrlKey) {
+                    $('#rocket')
+                        .css({ 'transform': 'rotate(45deg)' })
+                        .animate({ top: "-=100px", left: "+=100px" }, 500);
+                } else if (e.shiftKey) {
+                    $('#rocket')
+                        .css({ 'transform': 'rotate(135deg)' })
+                        .animate({ top: "+=100px", left: "+=100px" }, 500);
+                } else {
+                    $('#rocket')
+                        .css({ 'transform': 'rotate(90deg)' })
+                        .animate({ left: "+=100px" }, 500);
+                }
                 break;
             case "ArrowUp":
-                $('#rocket').animate({top:"-=100px"}, 500);
+                $('#rocket')
+                    .css({ 'transform': 'rotate(0deg)' })
+                    .animate({ top: "-=100px" }, 500);
                 break;
             case "ArrowDown":
-                $('#rocket').animate({top:"+=100px"}, 500);
+                $('#rocket')
+                    .css({ 'transform': 'rotate(180deg)' })
+                    .animate({ top: "+=100px" }, 500);
                 break;
         }
+    });
+
+    $('#btn').click(function () {
+        $('#rocket').css({
+            'top': '0',
+            'left': '50%'
+        });
     });
 })
